ffb6fab0cb6cad65630a5584ae6422aa7d8daa73
3urdparty <3urdparty@gmail.com>
2025-02-05 16:42:51 +0800

    Removed console.logs

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -1,10 +1,8 @@
 import { Database } from '@/database.types'
 import { createClient } from '@supabase/supabase-js'
 
-console.log(import.meta.env)
 const PROJECT_URL = import.meta.env.VITE_SUPABASE_PROJECT_URL
 const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY
-console.log(import.meta.env)
 
 const options = {
   auth: {
