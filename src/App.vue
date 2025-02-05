3226d4ead87e64279539b55ec886c53dc6de43fb
3urdparty <3urdparty@gmail.com>
2025-02-05 17:15:17 +0800

    Added BaseLayout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import BaseLayout from '@/layouts/BaseLayout.vue'
+import AppShell from '@/layouts/AppShell.vue'
 </script>
 
 <template>
-  <RouterView />
+  <BaseLayout>
+    <AppShell>
+      <RouterView />
+    </AppShell>
+  </BaseLayout>
 </template>
